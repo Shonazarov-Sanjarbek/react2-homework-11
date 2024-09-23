b0e032e721857ce3edf41688b479167196edb7ba
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-09-23 10:03:18 +0500

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useCheckUserQueryQuery } from "./redux/api/authApi";
+import RouteController from "./routes";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "./redux/slices/authSlices";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const location = useLocation();
+  const navigete = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+  const { data, isError, isLoading, isSuccess } = useCheckUserQueryQuery();
+  useEffect(() => {
+    if (isError) {
+      if (
+        !(location.pathname === "/auth/sign-up") &&
+        !(location.pathname === "/auth/sign-in") &&
+        !(location.pathname === "/auth/otp")
+      ) {
+        navigete(`/auth/sign-in?callback-url=${location.pathname}`);
+      }
+    }
+    if (isSuccess) {
+      dispatch(setUser(data.payload));
+      console.log(user);
+    }
+  }, [isError, isSuccess, user]);
+  return <>{isLoading ? "Loadng. . ." : <RouteController />}</>;
 }
 
-export default App
+export default App;
